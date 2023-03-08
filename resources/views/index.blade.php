649ddf514fee5c4cf24298d7550bb5b216aafc67
mrdrawin <mrdrawin@abv.bg>
2023-03-08 11:03:14 +0100

    project imported

---
+++ resources/views/index.blade.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>My Web Page</title>
+  <link rel="stylesheet" href="{{ asset('css/bootstrap.min.css') }}">
+</head>
+<body class="bg-image" style="background-image: url('https://www.buffed.de/screenshots/original/2016/06/Bartender-2--1-buffed_b2article_artwork.jpg');">
+    <div class="jumbotron jumbotron-fluid" style="margin-bottom: 0">
+      <div class="container">
+        <h1 class="display-4">Items</h1>
+        <p class="lead">Nie wieder den Überblick verlieren!</p>
+      </div>
+    </div>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <!--<a class="navbar-brand" href="#">Item Manager</a>-->
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+                <li class="nav-item active">
+                    <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Suche</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Hinzufügen</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="/info">Infos</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+
+
+    @yield('main')
+
+    
+  <!-- Your HTML content goes here -->
+  <script src="{{ asset('js/jquery.min.js') }}"></script>
+  <script src="{{ asset('js/bootstrap.min.js') }}"></script>
+</body>
+</html>
---
+++ resources/views/info.blade.php
@@ -0,0 +1,18 @@
+@extends('index')
+
+@section('main')
+
+
+<div class="container" style="background-color:#e9ecef; height: 100vh; margin-top: 100px; padding: 100px;">
+      <h1>Über das Projekt</h1>
+      <p>Wir wollten nie wieder den Überblick über unsere Items verlieren! Daher haben wir uns enschlossen, dieses kleine Tool zu bauen.</p>
+      <h1>Unser Team</h1>
+      <ul>
+        <li>Sabrina Ziebarth</li>
+        <li>Der pinke Elefant auf ihrem Schreibtisch</li>
+        <li>Viel Tee. Sogar mit Zucker!</li>
+      </ul>
+</div>
+
+
+@stop
